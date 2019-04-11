b06c611ed0666978b14d042b075fc0e0b94b86b6
rvkeerthana <49307577+rvkeerthana@users.noreply.github.com>
2019-04-11 12:41:22 +0530

    b8.py

---
+++ b8.py
@@ -0,0 +1,11 @@
+num=int(input("enter the number:")
+hold=num
+sum=0
+if num<=0:
+print("enter a positive number")
+else:
+while num>0:
+sum=sum+num
+num=num-1:
+print("sum of first",hold,"natural number is:",sum)
+
