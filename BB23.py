eef9e6eb98a2f3a963107a2a9c2f851352d2ee86
rvkeerthana <49307577+rvkeerthana@users.noreply.github.com>
2019-05-09 11:26:45 +0530

    BB23.py

---
+++ BB23.py
@@ -0,0 +1,12 @@
+def min(list1,n):
+final_list=[]
+for i in range(0,n):
+min1=9999999;
+for j in range(len(list1)):
+if list1[j]<min1:
+min1=list[j]
+list1.remove(min);
+print(final_list)
+list=(4,78,34,10,8,21,11,231);
+n=2;
+min(list1,n)
